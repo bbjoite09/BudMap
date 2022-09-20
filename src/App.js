307b5403fd33063a4938e491175572832f22f4e9
Heeeon <bbjoite09@naver.com>
2022-09-20 20:32:42 +0900

    Feat : layout setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import styled from "styled-components";
 
 function App() {
-  return;
+  return (
+    <BackgroundStyle className="App">
+      <Container>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Container>
+    </BackgroundStyle>
+  );
 }
+
+const BackgroundStyle = styled.div`
+  background-color: #dcdcdc;
+`;
+
+const Container = styled.div`
+  max-width: 500px;
+  height: 100vh;
+  margin: 0 auto;
+  overflow-y: auto;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-color: white;
+`;
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div style={{ width: "100%", height: 300, backgroundColor: "pink" }}></div>
+    </>
+  );
+};
+
+export default Home;
